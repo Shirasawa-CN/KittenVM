cc63313b40b0942a33f95dbe8fd14739c0291abf
Shirasawa <3489840593@qq.com>
2022-11-05 12:21:25 +0800

    1.0.0 测试版1 添加stream流输入API,诚邀大家测试，晚些时候会发release

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,5 +1,6 @@
+use crate::vm::machine;
 use anyhow::{Ok, Result};
-use std::path::Path;
+use std::{path::Path, str::FromStr};
 
 enum SafeMode {
     Safe,
@@ -15,10 +16,36 @@ pub struct Mode {
     gc_mode: GCMode,
 }
 
-pub fn stream(code: String) -> Result<()> {
-    Ok(())
+pub struct API<T> {
+    vm: machine::KittenVM<T>,
 }
 
-pub fn file(_file_path: &Path) -> Result<()> {
-    Ok(())
+impl<
+        T: 'static
+            + Clone
+            + std::str::FromStr
+            + std::ops::Add<Output = T>
+            + std::ops::Sub<Output = T>
+            + std::ops::Mul<Output = T>
+            + std::ops::Div<Output = T>
+            + std::ops::Shl<Output = T>
+            + std::ops::Shr<Output = T>
+            + std::ops::BitXor<Output = T>
+            + std::ops::BitOr<Output = T>
+            + std::ops::BitAnd<Output = T>,
+    > API<T>
+{
+    pub fn file(_file_path: &Path) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn stream(&mut self, code: String) -> Result<(), anyhow::Error>
+    where
+        <T as FromStr>::Err: Sync,
+        <T as FromStr>::Err: std::error::Error,
+        <T as FromStr>::Err: Send,
+    {
+        let result = machine::KittenVM::matcher(&mut self.vm, code);
+        result
+    }
 }
